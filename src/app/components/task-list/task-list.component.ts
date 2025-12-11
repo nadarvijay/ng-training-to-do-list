a65d3c5313c3f57f7a7fae20f715b4178b0c67e2
Nadar.Murugan <nadar.murugan@ril.com>
2025-12-11 16:19:00 +0530

    Header UI Complete: Made changes in App styling Made changes in Header styling Added Footer Component

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { TaskDataTableComponent } from "../task-data-table/task-data-table.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [HeaderComponent, TaskDataTableComponent],
+  imports: [HeaderComponent, TaskDataTableComponent, FooterComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
